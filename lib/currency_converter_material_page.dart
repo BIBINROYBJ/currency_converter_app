8dee06a70f9d9a22ace8248ae58d5bec374cd0b5
BIBINROYBJ <bjbibin.royip@gmail.com>
2024-07-31 23:56:33 +0530

    commit

---
+++ lib/currency_converter_material_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterialPage extends StatelessWidget {
+  const CurrencyConverterMaterialPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        // strokeAlign: BorderSide.strokeAlignInside,
+        color: Colors.black,
+        width: 2.0,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.circular(5),
+    );
+
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 78, 147, 107),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "0",
+              style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.w700,
+                  color: Color.fromARGB(255, 19, 19, 19)),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: TextField(
+                style: const TextStyle(
+                  color: Color.fromRGBO(0, 0, 0, 1),
+                ),
+                // we have decorated the input field
+                decoration: InputDecoration(
+                  hintText: "Please enter the amt in USD",
+                  hintStyle: const TextStyle(color: Colors.white60),
+                  prefixIcon: const Icon(Icons.monetization_on_outlined),
+                  prefixIconColor: const Color.fromARGB(179, 0, 0, 0),
+                  filled: true,
+                  fillColor: const Color.fromARGB(255, 173, 177, 58),
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+                // keyboard allow only numbers
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import './currency_converter_material_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// type of Widgets
+
+// 1. stateless widget
+// 2. stateful widget
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return const MaterialApp(
+      home: CurrencyConverterMaterialPage()
+      
+    );
+    
+  }
+}
